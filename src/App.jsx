fdaed16f3ffd6a674c834fbfac51764181b8d541
spawn1k1234 <vladislav.sidorov2008@gmail.com>
2025-06-22 13:16:45 +0200

    qwe

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import FeedbackOptions from "./components/FeedbackOptions";
+import Statistics from "./components/Statistics";
+import Section from "./components/Section";
+import Notification from "./components/Notification";
+
+const App = () => {
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
+
+  const handleFeedback = (type) => {
+    setFeedback((prev) => ({
+      ...prev,
+      [type]: prev[type] + 1,
+    }));
+  };
+
+  const countTotalFeedback = () => {
+    return feedback.good + feedback.neutral + feedback.bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
+    return total ? Math.round((feedback.good / total) * 100) : 0;
+  };
+
+  const total = countTotalFeedback();
+  const positivePercentage = countPositiveFeedbackPercentage();
+
+  return (
+    <div className="cont">
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={["good", "neutral", "bad"]}
+          onLeaveFeedback={handleFeedback}
+        />
+      </Section>
+
+      <Section title="Statistics">
+        {total > 0 ? (
+          <Statistics
+            good={feedback.good}
+            neutral={feedback.neutral}
+            bad={feedback.bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
+        ) : (
+          <Notification message="неттттууууу" />
+        )}
+      </Section>
+    </div>
+  );
+};
+
+export default App;
